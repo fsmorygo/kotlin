075d22fcdbf766b0feddc4226732faf5461d3e42
Philipp Smorygo <smorygo.philipp@gmail.com>
2018-12-11 00:45:34 +0300

    Build AppCode plugin with gradle bundled

--- prepare/appcode-plugin/build.gradle.kts
+++ prepare/appcode-plugin/build.gradle.kts
@@ -2,6 +2,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import com.github.jk1.tcdeps.KotlinScriptDslAdapter.teamcityServer
 import com.github.jk1.tcdeps.KotlinScriptDslAdapter.tc
 import org.gradle.kotlin.dsl.support.zipTo
+import java.util.regex.Pattern
 
 apply {
     plugin("kotlin")
@@ -21,6 +22,7 @@ repositories {
     }
 }
 
+val kotlinVersion = rootProject.extra["kotlinVersion"] as String
 val cidrPluginDir: File by rootProject.extra
 val appcodePluginDir: File by rootProject.extra
 val appcodeVersion = rootProject.extra["versions.appcode"] as String
@@ -28,27 +30,36 @@ val appcodeVersionRepo = rootProject.extra["versions.appcode.repo"] as String
 
 val cidrPlugin by configurations.creating
 val platformDepsZip by configurations.creating
+val cidrGradleZip by configurations.creating
 
 val pluginXmlPath = "META-INF/plugin.xml"
+val javaPsiXmlPath = "META-INF/JavaPsiPlugin.xml"
+val javaPluginXmlPath = "META-INF/JavaPlugin.xml"
 val platformDepsJarName = "kotlinNative-platformDeps.jar"
+val cidrGradleJarName = "gradle.jar"
+val resourcesJarName = "resources_en.jar"
+val pluginXmlLocation = File(buildDir, "pluginXml")
 
 // Do not rename, used in JPS importer
-val projectsToShadow by extra(listOf(
-    ":kotlin-ultimate:cidr-native",
-    ":kotlin-ultimate:appcode-native"))
-
+val projectsToShadow by extra(
+    listOf(
+        ":kotlin-ultimate:cidr-native",
+        ":kotlin-ultimate:appcode-native"
+    )
+)
 
 dependencies {
     cidrPlugin(project(":prepare:cidr-plugin"))
     platformDepsZip(tc("$appcodeVersionRepo:$appcodeVersion:OC-plugins/kotlinNative-platformDeps-$appcodeVersion.zip"))
+    cidrGradleZip(tc("$appcodeVersionRepo:$appcodeVersion:OC-plugins/gradle-$appcodeVersion.zip"))
 }
 
-val kotlinPluginXml by tasks.creating {
-    inputs.files(cidrPlugin)
-    outputs.files(fileFrom(buildDir, name, "META-INF/KotlinPlugin.xml"))
+fun renamePluginXml(plugin: Configuration, jarFile: File, toName: String) = tasks.creating {
+    inputs.files(plugin)
+    outputs.files(fileFrom(buildDir, name, "META-INF/$toName"))
 
     doFirst {
-        val pluginXmlText = zipTree(inputs.files.singleFile)
+        val pluginXmlText = zipTree(jarFile)
             .matching { include(pluginXmlPath) }
             .singleFile
             .readText()
@@ -56,9 +67,64 @@ val kotlinPluginXml by tasks.creating {
     }
 }
 
+val kotlinPluginXml by renamePluginXml(cidrPlugin, cidrPlugin.singleFile, "KotlinPlugin.xml")
+
+val gradlePluginXml by renamePluginXml(
+    cidrGradleZip,
+    zipTree(cidrGradleZip.singleFile).matching { include("**/$cidrGradleJarName") }.singleFile,
+    "GradlePlugin.xml"
+)
+
+val cidrGradleJar by task<Zip> {
+    archiveName = cidrGradleJarName
+    from(gradlePluginXml) { into("META-INF") }
+    val cidrGradleJar = zipTree(cidrGradleZip.singleFile).matching { include("**/$cidrGradleJarName") }.singleFile
+    from(zipTree(cidrGradleJar)) {
+        exclude(pluginXmlPath)
+    }
+}
+
+val resourcesJar by task<Zip> {
+    archiveName = resourcesJarName
+    val cidrJar = zipTree(cidrGradleZip.singleFile).matching { include("**/$resourcesJarName") }.singleFile
+    from(zipTree(cidrJar))
+    val platformDepsJar = zipTree(platformDepsZip.singleFile).matching { include("**/$resourcesJarName") }.singleFile
+    from(zipTree(platformDepsJar))
+}
+
+
+val preparePluginXml by task<Copy> {
+    dependsOn(":kotlin-ultimate:appcode-native:assemble")
+
+    val cidrPluginVersion = project.findProperty("cidrPluginVersion") as String? ?: "beta-1"
+    val appcodePluginVersion = "$kotlinVersion-AppCode-$cidrPluginVersion-$appcodeVersion"
+
+    inputs.property("appcodePluginVersion", appcodePluginVersion)
+
+    from(project(":kotlin-ultimate:appcode-native").mainSourceSet.output.resourcesDir) { include(pluginXmlPath) }
+    into(pluginXmlLocation)
+
+    val sinceBuild =
+        if (appcodeVersion.matches(Regex("\\d+\\.\\d+"))) appcodeVersion else appcodeVersion.substring(0, appcodeVersion.lastIndexOf('.'))
+    val untilBuild = appcodeVersion.substring(0, appcodeVersion.lastIndexOf('.')) + ".*"
+
+    filter {
+        it
+            .replace(
+                "<!--idea_version_placeholder-->",
+                "<idea-version since-build=\"$sinceBuild\" until-build=\"$untilBuild\"/>"
+            )
+            .replace(
+                "<!--version_placeholder-->",
+                "<version>$appcodePluginVersion</version>"
+            )
+    }
+}
+
 val jar = runtimeJar {
     archiveName = "kotlin-plugin.jar"
     dependsOn(cidrPlugin)
+    dependsOn(preparePluginXml)
     from(kotlinPluginXml) { into("META-INF") }
 
     from {
@@ -69,7 +135,38 @@ val jar = runtimeJar {
 
     for (p in projectsToShadow) {
         dependsOn("$p:classes")
-        from(getSourceSetsFrom(p)["main"].output)
+        from(getSourceSetsFrom(p)["main"].output) {
+            exclude(pluginXmlPath)
+        }
+    }
+    from(pluginXmlLocation) { include(pluginXmlPath) }
+}
+
+fun Zip.includePatched(fileToMarkers: Map<String, List<String>>) {
+    val notDone = mutableSetOf<Pair<String, String>>()
+    fileToMarkers.forEach { (path, markers) ->
+        for (marker in markers) {
+            notDone += path to marker
+        }
+    }
+
+    eachFile {
+        val markers = fileToMarkers[this.sourcePath] ?: return@eachFile
+        this.filter {
+            var data = it
+            for (marker in markers) {
+                val newData = data.replace(("^(.*" + Pattern.quote(marker) + ".*)$").toRegex(), "<!-- $1 -->")
+                data = newData
+                notDone -= path to marker
+            }
+            data
+        }
+    }
+    doLast {
+        check(notDone.size == 0) {
+            "Filtering failed for: " +
+                    notDone.joinToString(separator = "\n") { (file, marker) -> "file=$file, marker=`$marker`" }
+        }
     }
 }
 
@@ -78,6 +175,12 @@ val platformDepsJar by task<Zip> {
     val platformDepsJar = zipTree(platformDepsZip.singleFile).matching { include("**/$platformDepsJarName") }.singleFile
     from(zipTree(platformDepsJar)) {
         exclude(pluginXmlPath)
+        includePatched(
+            mapOf(
+                javaPsiXmlPath to listOf("implementation=\"org.jetbrains.uast.java.JavaUastLanguagePlugin\""),
+                javaPluginXmlPath to listOf("implementation=\"com.intellij.spi.SPIFileTypeFactory\"")
+            )
+        )
     }
 }
 
@@ -88,7 +191,15 @@ task<Copy>("appcodePlugin") {
     from(platformDepsJar) { into("lib") }
     from(zipTree(platformDepsZip.singleFile).files) {
         exclude("**/$platformDepsJarName")
+        exclude("**/$resourcesJarName")
         into("lib")
     }
+    from(cidrGradleJar) { into("lib") }
+    from(zipTree(cidrGradleZip.singleFile).files) {
+        exclude("**/$cidrGradleJarName")
+        exclude("**/$resourcesJarName")
+        into("lib")
+    }
+    from(resourcesJar) { into("lib") }
     from(File(project(":kotlin-ultimate:appcode-native").projectDir, "templates")) { into("templates") }
 }
